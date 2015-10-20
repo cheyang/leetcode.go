bbbd755dfc2fc78cd0046951ada71e8ed4f1d0cb
cheyang <cheyang@163.com>
2015-10-20 17:53:43 +0800

    fix typo

--- src/remove_element_27/main.go
+++ src/remove_element_27/main.go
@@ -1,33 +1,27 @@
-package remove_element_27 
+package remove_element_27
 
-import (
-	
-)
+import ()
 
+func RemoveElement(array []int, value int) int {
 
-
-func RemoveElement(array []int, value int) int{
-	
 	var currentLength int = len(array)
-	
+
 	var startPos = 0
-	
-	JUMP:
+
+JUMP:
 	for startPos < (currentLength - 1) {
-		
-		if array[startPos] == value{
-			
+
+		if array[startPos] == value {
+
 			array[startPos] = array[currentLength-1]
-			
-			currentLength --
-			
+
+			currentLength--
+
 			goto JUMP
 		}
-		
-		
-		startPos ++
+
+		startPos++
 	}
-	
+
 	return currentLength
 }
-
--- src/remove_element_27/main_test.go
+++ src/remove_element_27/main_test.go
@@ -1,50 +1,40 @@
 package remove_element_27
 
 import (
-    "testing"
-    . "github.com/smartystreets/goconvey/convey"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
 )
 
 func Test_RemoveElement(t *testing.T) {
-	
-	Convey("remove elements", t , func(){
-			
-			if	length := RemoveElement([]int{2, 7, 11, 15}, 9){
-				
-				So(length, ShouldEqual, 4)
-				}
-			
-			})
-		
-	}
-}
 
+	Convey("remove elements", t, func() {
+
+		length := RemoveElement([]int{2, 7, 11, 15}, 9)
+
+		So(length, ShouldEqual, 5)
+	})
+}
 
 func Test_RemoveElement1(t *testing.T) {
-	
-	Convey("remove elements", t , func(){
-			
-			if	length := RemoveElement([]int{1, 2, 7, 9,  11, 15}, 9){
-				
-				So(length, ShouldEqual, 5)
-				}
-			
-			})
-		
-	}
+
+	Convey("remove elements", t, func() {
+
+		length := RemoveElement([]int{1, 2, 7, 9, 11, 15}, 9)
+
+		So(length, ShouldEqual, 5)
+
+	})
+
 }
 
+func Test_RemoveElement2(t *testing.T) {
 
-func Test_RemoveElement3(t *testing.T) {
-	
-	Convey("remove elements", t , func(){
-			
-			if	length := RemoveElement([]int{1, 9, 9, 9,  11, 9}, 9){
-				
-				So(length, ShouldEqual, 2)
-				}
-			
-			})
-		
-	}
+	Convey("remove elements", t, func() {
+
+		length := RemoveElement([]int{1, 9, 9, 9, 11, 9}, 9)
+
+		So(length, ShouldEqual, 2)
+
+	})
+
 }

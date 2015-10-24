707d884bdc2b87bb41e34552608f711b3df1eac8
cheyang <cheyang@163.com>
2015-10-24 15:24:01 +0800

    result declared and not used

--- src/plus_one_66/main_test.go
+++ src/plus_one_66/main_test.go
@@ -15,24 +15,24 @@ func Test_PlusOne(t *testing.T) {
 
 			expect_data := []int{1, 2, 3, 4, 6}
 
-			So(expect_data[0], ShouldEqual, 1)
-			So(expect_data[1], ShouldEqual, 2)
-			So(expect_data[2], ShouldEqual, 3)
-			So(expect_data[3], ShouldEqual, 4)
-			So(expect_data[4], ShouldEqual, 6)
+			So(expect_data[0], ShouldEqual, result[0])
+			So(expect_data[1], ShouldEqual, result[1])
+			So(expect_data[2], ShouldEqual, result[2])
+			So(expect_data[3], ShouldEqual, result[3])
+			So(expect_data[4], ShouldEqual, result[4])
 		})
 
 		Convey("Plus One with carry-over", func() {
 
 			result := PlusOne([]int{9, 9, 9, 9})
 
-			expect_data := []int{1, 0, 0, 0, 0}
+//			expect_data := []int{1, 0, 0, 0, 0}
 
-			So(expect_data[0], ShouldEqual, 1)
-			So(expect_data[1], ShouldEqual, 0)
-			So(expect_data[2], ShouldEqual, 0)
-			So(expect_data[3], ShouldEqual, 0)
-			So(expect_data[4], ShouldEqual, 0)
+			So(expect_data[0], ShouldEqual, result[0])
+			So(expect_data[1], ShouldEqual, result[1])
+			So(expect_data[2], ShouldEqual, result[2])
+			So(expect_data[3], ShouldEqual, result[3])
+			So(expect_data[4], ShouldEqual, result[4])
 		})
 
 	})

4cecab99d3b437e048bce4d71d0c7f11e657d6f7
cheyang <cheyang@163.com>
2015-10-28 07:26:37 +0800

    return code

--- src/search_for_a_range_34/main.go
+++ src/search_for_a_range_34/main.go
@@ -6,31 +6,53 @@ import (
 
 func SearchRange(array []int, target int) (result []int){
 	
-	tempArray := array
 	
-	mid := len(tempArray)/2
+	
+	mid := len(array)/2
+	
+	start := 0
+	
+	end := len(array) - 1
 	
 	result = []int{-1, -1}
 	
-	for mid > -1 || mid + 1 < len(tempArray){
+	for mid>start || mid<end {
 		
-		part1 := tempArray[0:mid]
-		
-		part2 := tempArray[mid:len(tempArray)]
-		
-		
-		if target >= part2[0] {
+		if array[mid] > target{
 			
-			tempArray = part2
+			end = mid -1
 			
 		}else{
-			tempArray = part1
+			
+			start = mid 
 		}
 		
-		
-		mid = len(tempArray)/2
+		mid = start + (end - start)/2
 	}
 	
 	
+	switch target {
+		case array[start]:
+			if start - 1 >= 0 && array[start - 1] == target{
+				result[0] = start -1
+				result[1] = start
+			} else if start + 1 < len(array) && array[start+1] == target{
+				result[0] = start
+				result[1] = start + 1
+			}else{
+				result[0] = start
+			}
+		case array[end]:
+			if end + 1 < len(array) && array[end+1] == target {
+				result[0] = end
+				result[1] = end + 1
+			}else if end - 1 < len(array) && array[end -1] == target{
+				result[0] = end - 1
+				result[1] = end 
+			}else{
+				result[0] = end
+			}
+	}
 	
+	return
 }

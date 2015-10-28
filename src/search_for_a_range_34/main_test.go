6cbe144425e3482c5850816fe821de86566fc592
cheyang <cheyang@163.com>
2015-10-28 18:13:51 +0800

    add 8

--- src/search_for_a_range_34/main_test.go
+++ src/search_for_a_range_34/main_test.go
@@ -9,7 +9,7 @@ func Test_SearchRange(t *testing.T) {
 		
 		Convey("Search Range", t, func(){
 				
-				result := SearchRange([]int{5, 7, 7, 8, 8, 10})
+				result := SearchRange([]int{5, 7, 7, 8, 8, 10}, 8)
 				So(result, ShouldResemble, []int{3,4})
 				})
 }

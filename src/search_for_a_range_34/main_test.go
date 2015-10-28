6affdff7d3f32f46856e4ee837208092a845691d
cheyang <cheyang@163.com>
2015-10-28 17:31:57 +0800

    test code

---
+++ src/search_for_a_range_34/main_test.go
@@ -0,0 +1,16 @@
+package search_for_a_range_34
+
+import (
+    "testing"
+    . "github.com/smartystreets/goconvey/convey"
+)
+
+func Test_SearchRange(t *testing.T) {
+		
+		Convey("Search Range", t, func(){
+				
+				result := SearchRange([]int{5, 7, 7, 8, 8, 10})
+				So(result, ShouldResemble, []int{3,4})
+				})
+}
+

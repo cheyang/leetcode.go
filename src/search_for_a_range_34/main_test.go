de80e2d291acfc7a557132fe8b038e31f499e831
cheyang <cheyang@163.com>
2015-10-28 18:44:35 +0800

    range 3

--- src/search_for_a_range_34/main_test.go
+++ src/search_for_a_range_34/main_test.go
@@ -20,6 +20,12 @@ func Test_SearchRange(t *testing.T) {
 							So(result, ShouldResemble, []int{0,-1})
 							
 							})
+					
+					Convey("Search Range 3", func(){
+							result := SearchRange([]int{5, 7, 7, 8, 8, 10}, 7)
+							So(result, ShouldResemble, []int{0,-1})
+							
+							})
 				})
 }
 

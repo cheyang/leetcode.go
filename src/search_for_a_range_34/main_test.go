163e59efd0fdd97836e4d96d85b2f56d5773d518
cheyang <cheyang@163.com>
2015-10-28 18:43:26 +0800

    with edge

--- src/search_for_a_range_34/main_test.go
+++ src/search_for_a_range_34/main_test.go
@@ -7,10 +7,19 @@ import (
 
 func Test_SearchRange(t *testing.T) {
 		
-		Convey("Search Range", t, func(){
+		Convey("Search Range ", t, func(){
 				
-				result := SearchRange([]int{5, 7, 7, 8, 8, 10}, 8)
-				So(result, ShouldResemble, []int{3,4})
+					Convey("Search Range middle", func(){
+							result := SearchRange([]int{5, 7, 7, 8, 8, 10}, 8)
+							So(result, ShouldResemble, []int{3,4})
+							
+							})
+					
+					Convey("Search Range 2", func(){
+							result := SearchRange([]int{5, 7, 7, 8, 8, 10}, 5)
+							So(result, ShouldResemble, []int{0,-1})
+							
+							})
 				})
 }
 

065788fb4386196a29065d3134197f248946a6c5
cheyang <cheyang@163.com>
2015-11-03 22:00:02 +0800

    search insert pos

---
+++ src/search_insert_position_35/main.go
@@ -0,0 +1,19 @@
+package search_insert_position_35
+
+import (
+
+)
+
+func SearchInsertTarget(nums []int, target int) (pos int){
+	
+	
+	mid := len(nums)/2
+	
+	start := 0
+	
+	end :=  len(nums) - 1
+	
+	
+	for end - start >= 1
+	
+}

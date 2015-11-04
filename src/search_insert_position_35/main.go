d11f8fe6b55548f9d5268d8f5fa0be3199baab33
cheyang <cheyang@163.com>
2015-11-04 15:14:54 +0800

    search insert position

--- src/search_insert_position_35/main.go
+++ src/search_insert_position_35/main.go
@@ -1,19 +1,69 @@
 package search_insert_position_35
 
-import (
+import ()
 
-)
+func SearchInsertTarget(nums []int, target int) (pos int) {
 
-func SearchInsertTarget(nums []int, target int) (pos int){
-	
-	
-	mid := len(nums)/2
-	
+	pos = -1
+
+	mid := len(nums) / 2
+
 	start := 0
-	
-	end :=  len(nums) - 1
-	
-	
-	for end - start >= 1
-	
+
+	end := len(nums) - 1
+
+	for end-start > 0 {
+
+		if nums[mid] == target {
+			pos = mid
+			break
+		}
+
+		if target > nums[end] {
+			pos = end
+		}
+
+		switch {
+		case nums[mid] == target:
+			pos = mid
+			break
+		case target > nums[end]:
+			pos = end + 1
+			break
+		case target == nums[end]:
+			pos = end
+			break
+		case target <= nums[start]:
+			pos = start
+			break
+		}
+
+		if nums[mid] > target {
+			end = mid
+		} else {
+			start = mid
+		}
+
+		mid = (start + end) / 2
+	}
+
+	if pos == -1 {
+		switch {
+		case nums[mid] == target:
+			pos = mid
+		case mid == 0:
+			pos = mid + 1
+		case mid == len(nums)-1:
+			pos = mid - 1
+		default:
+			if nums[mid] > target {
+				pos = mid - 1
+			} else {
+				pos = mid + 1
+			}
+
+		}
+	}
+
+	return pos
 }
---
+++ src/search_insert_position_35/main_test.go
@@ -0,0 +1,57 @@
+package search_insert_position_35
+
+import (
+    "testing"
+    
+    . "github.com/smartystreets/goconvey/convey"
+)
+
+func Test_SearchInsertTarget(t *testing.T) {
+	
+	Convey("Search Insert Target Test", t , func(){
+			
+			Convey("compare [1,3,5,6], 5 → 2", func(){
+					
+					pos := SearchInsertTarget([]int{1,3,5,6}, 5)
+					
+					So(pos, ShouldEqual, 2)
+					
+					})
+			
+			Convey("compare [1,3,5,6], 2 → 1", func(){
+					
+					pos := SearchInsertTarget([]int{1,3,5,6}, 2)
+					
+					So(pos, ShouldEqual, 1)
+					
+					})
+			
+			Convey("compare [1,3,5,6], 7 → 4", func(){
+					
+					pos := SearchInsertTarget([]int{1,3,5,6}, 7)
+					
+					So(pos, ShouldEqual, 4)
+					
+					})
+			
+			Convey("compare [1,3,5,6], 0 → 0 ", func(){
+					
+					pos := SearchInsertTarget([]int{1,3,5,6}, 0)
+					
+					So(pos, ShouldEqual, 0)
+					
+					})
+			
+			
+			Convey("compare [1,3,5,6], 6 → 3 ", func(){
+					
+					pos := SearchInsertTarget([]int{1,3,5,6}, 6)
+					
+					So(pos, ShouldEqual, 3)
+					
+					})
+			
+			})
+
+}
+
